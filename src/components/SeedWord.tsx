b6cb792f1ed0b5f4b73d5e64740ca436594a460a
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2022-12-15 04:35:46 +0700

    fix: plus 1 to display the correct index on UI

--- src/components/SeedWord.tsx
+++ src/components/SeedWord.tsx
@@ -9,7 +9,7 @@ export const SeedWord = ({ seedWord }: Props) => {
   return (
     <>
       <div className="flex h-5 w-5 items-center justify-center rounded-full bg-accent-blue-3 text-center text-xs">
-        <p className="text-accent-blue-1">{index}</p>
+        <p className="text-accent-blue-1">{index + 1} </p>
       </div>
       <p className="whitespace-nowrap text-sm text-neutral-1">{name}</p>
     </>
